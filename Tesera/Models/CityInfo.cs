7afb0b655677aa67b1348884c3520f2cb4fca35c
Vasilii <pimenovasya@gmail.com>
2024-02-14 18:20:15 +0300

    Added class CityInfo

---
+++ Tesera/Models/CityInfo.cs
@@ -0,0 +1,16 @@
+﻿using Tesera.Types.Enums;
+
+namespace Tesera.Models
+{
+	public class CityInfo
+	{
+		public string? Type;
+		public string? Value;
+		public string? Alias;
+		public long TeseraId;
+		public string? Name;
+		public string? Name2;
+
+		public TeseraType GetTeseraType() => Enum.TryParse(typeof(TeseraType), this.Type, out object? result) ? (TeseraType)result : Types.Enums.TeseraType.Unknown;
+	}
+}
