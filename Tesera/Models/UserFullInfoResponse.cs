517df7e312ffd4db646066d99905892fe1a2d0d8
Vasilii <pimenovasya@gmail.com>
2024-02-14 18:21:32 +0300

    Added class UserFullInfoResponse

---
+++ Tesera/Models/UserFullInfoResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Tesera.Models
+{
+	public class UserFullInfoResponse
+	{
+		public readonly UserFullInfo User;
+		public int GamesInCollection;
+		public readonly IEnumerable<CollectionGameInfo> Collection;
+
+		public UserFullInfoResponse(UserFullInfo user, IEnumerable<CollectionGameInfo> collection)
+		{
+			this.User = user;
+			this.Collection = collection;
+		}
+	}
+}
