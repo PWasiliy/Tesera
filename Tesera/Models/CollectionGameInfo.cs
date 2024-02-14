5b75b7be2eafe4018ce42ad81aeeab3cce8fe99c
Vasilii <pimenovasya@gmail.com>
2024-02-14 18:21:14 +0300

    Added class CollectionGameInfo

---
+++ Tesera/Models/CollectionGameInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace Tesera.Models
+{
+	public class CollectionGameInfo
+	{
+		public long RelationId;
+		public DateTime CreationDateUtc;
+		public DateTime EventDate;
+		public string? Comment;
+		public readonly GameInfoShort Game;
+
+		public CollectionGameInfo(GameInfoShort game) => this.Game = game;
+	}
+}
