0ca601db0fb69e92e1b885cae9b23bd1902b8a6a
Vasilii <pimenovasya@gmail.com>
2024-02-14 18:19:58 +0300

    Added enum TeseraType

---
+++ Tesera/Types/Enums/TeseraType.cs
@@ -0,0 +1,8 @@
+﻿namespace Tesera.Types.Enums
+{
+	public enum TeseraType
+	{
+		Unknown, Game, Company, Award, Photo, Person, News, Article, Project, Event, VideoExternal, Club, Journal, List, ListElement, Thought, NewsRss, User, Country, City,
+		Sale, Purchase, Comment, File, Link, VideoInternal, CollectionOwn, Custom, GameRelated, GameArticle, GameNews, GameJournal, GameThought, Feed, Messages, Reports, Post
+	}
+}
